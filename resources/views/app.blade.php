8a3011029ad41ad71bf5477659e9e4e86eedfb34
Sergi Tur Badenas <sergitur@ebretic.com>
2015-03-21 17:49:31 +0100

    Added font-awesome

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Laravel with Ace Admin template</title>
 
+    <!-- app.css is bootstrap.css (slightly modified by Ace template) compiled with less -->
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/font-awesome.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
